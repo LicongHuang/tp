bc44a07ee33ee40357c212881da5b7157c2f1841
LicongHuang <alexliconghuang@gmail.com>
2023-10-02 22:58:38 +0800

    Remove markdown

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -78,9 +78,6 @@ public class AddressBookParser {
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
 
-        case JokeCommand.COMMAND_WORD:
-            return new JokeCommand();
-
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
